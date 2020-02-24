1278a0fc16fd8bba552c82f0412765b0b5ec9154
Pablo Melendez <pablofer.melendez@gmail.com>
2020-02-24 12:08:01 -0500

    conf redux, sagas, router in state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
-
-import './App.css';
 import {
   BrowserRouter, Route, Switch, Redirect,
 } from 'react-router-dom';
-
-import HomeContainer from './containers/home';
-import Page2Container from './containers/page2';
-import Page3Container from './containers/page3';
 
 function App() {
   return (
@@ -15,13 +9,7 @@ function App() {
       <div className="App">
         <Switch>
           <Route exact path="/">
-            <HomeContainer />
-          </Route>
-          <Route exact path="/page2">
-            <Page2Container />
-          </Route>
-          <Route exact path="/page3">
-            <Page3Container />
+            <div>Hola Mundo..!</div>
           </Route>
           <Redirect to="/" />
         </Switch>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
+import { history, store } from './redux/store';
+import theme from './config/theme';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const FullApp = () => (
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </Provider>
+  </ThemeProvider>
+);
+
+ReactDOM.render(<FullApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
